4c7491807760902948ebe477ae1f5cf97097ec5e
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-03-25 14:57:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,10 @@ countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mal
 next_country = "Niger"
 countries_in_western_africa.push(next_country)
 end
+
+
+def using_push(countries_in_western_africa, next_country)
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+next_country = "Niger"
+countries_in_western_africa.unshift(next_country)
+end
